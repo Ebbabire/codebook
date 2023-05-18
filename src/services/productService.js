a95172fe12e37b01a70a11503d35c26d80a22971
Ebba Birhanu <ebba.birhanu@gmail.com>
2023-05-18 19:50:14 +0300

    initial commit

---
+++ src/services/productService.js
@@ -0,0 +1,32 @@
+export async function getProductList(searchTerm) {
+  const response = await fetch(
+    `${process.env.REACT_APP_HOST}/products?name_like=${
+      searchTerm ? searchTerm : ""
+    }`
+  );
+  if (!response.ok) {
+    throw { message: response.statusText, status: response.status }; //eslint-disable-line
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function getProduct(id) {
+  const response = await fetch(`${process.env.REACT_APP_HOST}/products/${id}`);
+  if (!response.ok) {
+    throw { message: response.statusText, status: response.status }; //eslint-disable-line
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function getFeaturedList() {
+  const response = await fetch(
+    `${process.env.REACT_APP_HOST}/featured_products`
+  );
+  if (!response.ok) {
+    throw { message: response.statusText, status: response.status }; //eslint-disable-line
+  }
+  const data = await response.json();
+  return data;
+}
